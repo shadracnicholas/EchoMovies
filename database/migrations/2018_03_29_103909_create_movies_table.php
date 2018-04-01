91e4ee0ba4575cd822d22b0bfc115bbe32017098
= <=>
2018-04-02 01:36:10 +0500

    Bug Fixed For database

--- database/migrations/2018_03_29_103909_create_movies_table.php
+++ database/migrations/2018_03_29_103909_create_movies_table.php
@@ -16,16 +16,16 @@ class CreateMoviesTable extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->increments('id');
             $table->string('movie_title');
-            $table->text('movie_description');
-            $table->integer('movie_rating');
+            $table->float('movie_rating');
             $table->string('movie_category');
+            $table->text('movie_description');
             $table->string('movie_watch_time');
-            $table->string('movie_director_name');
             $table->string('movie_stars_name');
-            $table->string('movie_trailer_url')->nullable($value = true);
-            $table->string('movie_poster')->default('movies_images/default.jpg');
-            $table->float('movie_ticket_price');
             $table->date('movie_released_date');
+            $table->integer('movie_ticket_price');
+            $table->string('movie_director_name');
+            $table->string('movie_poster')->default('movies_images/default.jpg');
+            $table->string('movie_trailer_url')->nullable($value = true)->default('movies_images/default.mp3');;
             $table->timestamps();
         });
     }
