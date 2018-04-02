2141a3df35ae701899afeb763db4b711b66dc465
= <=>
2018-04-02 18:36:53 +0500

    Updated Work On Ticket Book Page

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -8,7 +8,7 @@
 	<h1>Movie Ticket Booking Widget</h1>
 	<!---728x90-->
 	<div class="main">
-		<h2>Multiplex Theatre Showing Screen 1</h2>
+		<h2>Multiplex Theatre Showing Screen</h2>
 		<div class="demo">
 			<div id="seat-map">
 				<div class="front">Select Screen Seat</div>
@@ -16,15 +16,17 @@
 			</div>
 			<div class="booking-details">
 				<ul class="book-left">
-					<li>Movie </li>
+					<li>Movie</li>
 					<li>Time</li>
 					<li>Date</li>
 					<li>Seats</li>
 					<li>Total</li>
+					<li>Name</li>
+					<li>Phone</li>
 				</ul>
 				<ul class="book-right">
 					<li>: {{$movie->movie_title}}</li>
-					: <select>
+					: <select  id="movie_time_select">
 						@for($i=1; $i<=4; $i++)
 						<option value="{{$schedule["schedule_day_time_$i"]}}">{{$schedule["schedule_day_time_$i"]}}</option>
 						@endfor	
@@ -32,13 +34,12 @@
 					<li>: {{ \Carbon\Carbon::parse($schedule["schedule_date"])->formatLocalized('%A, %d %B %Y') }}</li>
 					<li>: <span id="counter">0</span></li>
 					<li>: <b><i>Rs: </i><span id="total">0</span></b></li>
+					<li>: <input type="text" name="username" id="username" placeholder="Enter Your Name"></li>
+					<li>: <input type="tel" name="phone" id="phone" placeholder="03** *******"></li>
 				</ul>
 				<div class="clear"></div>
 				<ul id="selected-seats" class="scrollbar scrollbar1"></ul>
-
-
 				<button class="checkout-button">Book Now</button>
-				
 			</div>
 			<div style="clear:both"></div>
 		</div>
@@ -46,8 +47,10 @@
 @section('javascript')
 <!-- <script type="text/javascript" src="{{Route('home')}}/public/assets/pages/js/seatframe.js"></script> -->
 <script type="text/javascript">
+	
 		var price = {!! $schedule["movie_ticket_price"] !!}; //price
 		$(document).ready(function() {
+			// Logic
 			var $cart        = $('#selected-seats'), //Sitting Area
 				$counter = $('#counter'), //Votes
 				$total      = $('#total'); //Total money
@@ -80,7 +83,7 @@
 				click: function () { //Click event
 					if (this.status() == 'available') { //optional seat
 						$('<li>Row'+(this.settings.row+1)+' Seat'+this.settings.label+'</li>')
-						.attr('id', 'cart-item-'+this.settings.id)
+						.attr('id', this.settings.id)
 						.data('seatId', this.settings.id)
 						.appendTo($cart);
 
@@ -116,6 +119,50 @@
 			});
 			return total;
 		}
+		$(document).ready(function() {
+			// Check For Confirm Order Or Not
+			$(document).on('click', '.checkout-button', function(event) {
+				event.preventDefault();
+				/* Act on the event */
+				alertify.confirm("Are You Sure To Book This Order?.",
+					function(){
+						var tempArray = [];
+						// #selected-seats li
+						$('#selected-seats li').each(function () {
+						    // Getting the id of each div
+						    var id = $(this).attr('id');
+						    // Add to the array
+						    tempArray.push("'"+id+"'");
+						});
+						var data = {
+							time : $('#movie_time_select').val(),
+							counter : $('#counter').text(),
+							name : $('#username').val(),
+							phone : $('#phone').val(),
+							movieId:  {!! $schedule["movie_id"] !!},
+							scheduleId:  {!! $schedule["id"] !!},
+							seats: tempArray.join(","),
+						}
+						// #Validation
+						if (data.time == '') {
+							alertify.error('Please Select Movie Time!');
+						} else if(data.name == '') {
+							alertify.error('Please Enter Your Full Name!');
+						} else if(data.counter == '0') {
+							alertify.error('Please Select Your Seatss!');
+						} else if(data.phone == '') {
+							alertify.error('Please Enter Your Phone Number!');
+						}
+						 else {
+							console.log(data);
+						}
+						/*# Validation */
+					},
+					function(){
+						alertify.error('Order Canceled!');
+				});
+			});
+		});
 
 </script>
 @endsection
